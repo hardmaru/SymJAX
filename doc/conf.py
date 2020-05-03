dec6e3c56809b4ba8b96f1e3a226e0518456a687
RandallBalestriero <randallbalestriero@gmail.com>
2020-05-03 14:20:16 -0500

    weird

--- doc/conf.py
+++ doc/conf.py
@@ -34,7 +34,7 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-
+master_doc = 'index'
 extensions = [
     'sphinx_rtd_theme',
     'matplotlib.sphinxext.plot_directive',
